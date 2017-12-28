4be514d4a656961a371fe76fb76d1ef4d2d2caf3
YinJianxiang <YinJianxiang123@gmail.com>
2017-12-28 16:34:50 +0800

    ^_^

--- HUFFMAN/tmp/test/haha.cpp
+++ HUFFMAN/tmp/test/haha.cpp
@@ -4,21 +4,32 @@
 	> Mail: YinJianxiang123@gmail.com
 	> Created Time: 2017年12月27日 星期三 14时31分21秒
  ************************************************************************/
-
-#include<iostream>
-#include<cstdlib>
-#include <readline/readline.h>
-#include <readline/history.h>
-
+ #include<iostream>
+ #include<cstdio>
+ #include<cstdlib>
+ #include<cstring>
+ #include<sys/ioctl.h>
+ #include<sys/signal.h>
+ #include<sys/wait.h>
+ #include<sys/time.h>
+ #include<sys/types.h>
+ #include<sys/stat.h>
+ #include<readline/readline.h>
+ #include<readline/history.h>
+ #include<unistd.h>
+ #include<fcntl.h>
 using namespace std;
 
 int main(int argc,char *argv[]) {
-	char * a;
-	char b[256];
-	
-	a = readline(b);
+	char str_1[256] = "first\n";
+    char str_2[256] = "second\n";
+    char *t_1;
+    char *t_2;
     
-	free(a);
-	return 0;
+    t_1 = readline(str_1);
+    t_2 = readline(str_2);
+    printf("(%s) (%s)\n",t_1,t_2);
+
+    return 0;
 }
 
